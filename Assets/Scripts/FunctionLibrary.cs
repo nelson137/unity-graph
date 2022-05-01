4f87ba1c6d10f6e103ff4042d3b086fc0d2975f9
Nelson Earle <nelson.earle137@gmail.com>
2022-05-01 16:44:11 -0500

    add banded star torus

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -6,8 +6,8 @@ public static class FunctionLibrary
     public delegate Vector3 Function(float u, float v, float t);
 
     [SerializeField]
-    public enum FunctionName { SineWave, MultiSineWave, Ripple, Sphere, BandedSphere };
-    static Function[] functions = { SineWave, MultiSineWave, Ripple, Sphere, BandedSphere };
+    public enum FunctionName { SineWave, MultiSineWave, Ripple, Sphere, BandedSphere, BandedStarTorus };
+    static Function[] functions = { SineWave, MultiSineWave, Ripple, Sphere, BandedSphere, BandedStarTorus };
 
     public static Function GetFunction(FunctionName name)
     {
@@ -63,6 +63,18 @@ public static class FunctionLibrary
         var s = r * Cos(0.5f * PI * v);
         p.x = s * Sin(PI * u);
         p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 BandedStarTorus(float u, float v, float t)
+    {
+        Vector3 p;
+        var r1 = 0.7f + 0.1f * Sin(PI * (6f*u + 0.5f*t));
+        var r2 = 0.15f + 0.05f * Sin(PI * (8f*u + 4f*v + 2f*t));
+        var s = r1 + r2 * Cos(PI * v);
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
         return p;
     }
