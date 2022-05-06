4228a0e4a5f7cdad435e40f7c8a9a6605e95c871
Nelson Earle <nelson.earle137@gmail.com>
2022-05-05 23:00:34 -0500

    cleanup & improve frame stats script

--- Assets/Scripts/FrameStatsUI.cs
+++ Assets/Scripts/FrameStatsUI.cs
@@ -6,21 +6,45 @@ public class FrameStatsUI : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI textObject;
 
+    /// <summary>
+    /// The metric to display in the frame statistics panel.
+    /// </summary>
     public enum DisplayMetric
     {
         FrameRate,
         FrameDuration
     };
 
+    /// <summary>
+    ///
+    /// </summary>
     [SerializeField]
     DisplayMetric displayMetric;
 
+    /// <summary>
+    /// Number of seconds over which to collect frame statistics (stencil pattern).
+    /// </summary>
     [SerializeField, Range(0.1f, 5f)]
     float sampleDuration = 2f;
 
+    /// <summary>
+    /// Sum of frame durations over sample duration.
+    /// </summary>
     float duration = 0f;
-    float maxDuration = float.MinValue;
-    float minDuration = float.MaxValue;
+
+    /// <summary>
+    /// Longest frame duration and highest frame rate so far over current sample period.
+    /// </summary>
+    float worstDuration = float.MinValue;
+
+    /// <summary>
+    /// Shortest frame duration and lowest frame rate so far over current sample period.
+    /// </summary>
+    float bestDuration = float.MaxValue;
+
+    /// <summary>
+    /// Number of frames so far over sample period.
+    /// </summary>
     int count = 0;
 
     void Update()
@@ -28,33 +52,39 @@ public class FrameStatsUI : MonoBehaviour
         float currentDuration = Time.unscaledDeltaTime;
         duration += currentDuration;
         count += 1;
-        maxDuration = Mathf.Max(maxDuration, currentDuration);
-        minDuration = Mathf.Min(minDuration, currentDuration);
+        worstDuration = Mathf.Max(worstDuration, currentDuration);
+        bestDuration = Mathf.Min(bestDuration, currentDuration);
         if (duration >= sampleDuration)
         {
-            switch (displayMetric)
-            {
-                case DisplayMetric.FrameRate:
-                    textObject.SetText(
-                        "FPS\n{0:0}\n{1:0}\n{2:0}",
-                        1f / minDuration,
-                        count / duration,
-                        1f / maxDuration
-                    );
-                    break;
-                case DisplayMetric.FrameDuration:
-                    textObject.SetText(
-                        "MS\n{0:1}\n{1:1}\n{2:1}",
-                        1000f * minDuration,
-                        1000f * duration / count,
-                        1000f * maxDuration
-                    );
-                    break;
-            }
+            UpdateText();
             duration = 0f;
             count = 0;
-            minDuration = float.MaxValue;
-            maxDuration = float.MinValue;
+            bestDuration = float.MaxValue;
+            worstDuration = float.MinValue;
+        }
+    }
+
+    void UpdateText()
+    {
+        float avgDuration = duration / count;
+        switch (displayMetric)
+        {
+            case DisplayMetric.FrameRate:
+                textObject.SetText(
+                    "FPS\n{0:0}\n{1:0}\n{2:0}",
+                    1f / bestDuration,
+                    1f / avgDuration,
+                    1f / worstDuration
+                );
+                break;
+            case DisplayMetric.FrameDuration:
+                textObject.SetText(
+                    "MS\n{0:1}\n{1:1}\n{2:1}",
+                    1000f * bestDuration,
+                    1000f * avgDuration,
+                    1000f * worstDuration
+                );
+                break;
         }
     }
 }
